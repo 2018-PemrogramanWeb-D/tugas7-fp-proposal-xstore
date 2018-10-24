be06d6c1dda16b13d75c9f65868afaa9abc6806f
galihpribadi04 <galihpribadi04@gmail.com>
2018-10-24 12:05:04 +0700

    Tambah login

---
+++ login.php
@@ -0,0 +1,27 @@
+<html>
+<head>
+	<title>Login | Page</title>
+	<link rel="stylesheet" type="text/css" href="css/login.css">
+</head>  
+<body>
+	<section>
+		<div class="vid-container">
+  			<video id="Video1" class="bgvid back" autoplay="false" muted="muted" preload="auto" loop>
+      		<source src="http://shortcodelic1.manuelmasiacsasi.netdna-cdn.com/themes/geode/wp-content/uploads/2014/04/milky-way-river-1280hd.mp4.mp4" type="video/mp4">
+  			</video>
+  		<div class="inner-container">
+    		<video id="Video2" class="bgvid inner" autoplay="false" muted="muted" preload="auto" loop>
+    		<source src="http://shortcodelic1.manuelmasiacsasi.netdna-cdn.com/themes/geode/wp-content/uploads/2014/04/milky-way-river-1280hd.mp4.mp4" type="video/mp4">
+    		</video>
+		<div class="box-login">
+			<form action="" method="post">
+				<font>Login</font><br>
+				<input type="text" placeholder="Email/Username"><br>
+				<input type="password" placeholder="Password"><br>
+				<button>Login</button>
+				<p>Belum bergabung ? <span>Sign Up</span></p>
+			</form>
+		</div>
+	</section>
+</body>
+</html>
