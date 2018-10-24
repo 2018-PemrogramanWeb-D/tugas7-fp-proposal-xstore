b7c62be257254a5c7bea750694ed16084ffdf214
galihpribadi04 <galihpribadi04@gmail.com>
2018-10-24 21:39:55 +0700

    tambahan

---
+++ register.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Register Member</title>
+    <link rel="stylesheet" type="text/css" href="register.css">
+</head>
+
+<body>
+    <header>
+        <h3>Member Sign Up</h3>
+    </header>
+
+    <form action="" method="POST">
+
+        <fieldset>
+
+        <p>
+            <input type="text" name="nama" placeholder="Nama Depan" />
+        </p>
+
+        <p>
+            <input type="text" name="nama" placeholder="Nama Belakang" />
+        </p>
+
+        <p>
+            <input type="text" name="email" placeholder="Masukkan Email">
+        </p>
+
+        <p>
+            <input type="password" name="pass" placeholder="Password">
+        </p>
+
+        <button>
+            Register
+        </button>
+
+        </fieldset>
+
+    </form>
+
+    </body>
+</html>
