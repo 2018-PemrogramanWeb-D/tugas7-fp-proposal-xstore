bafcd4a7d36798e036e1099a189ab07931c12bae
Ivan Indrakusuma <ivan_indrakusuma@yahoo.com>
2018-11-01 22:12:22 +0700

    Update css register

--- register.php
+++ register.php
@@ -1,4 +1,4 @@
-<!DOCTYPE html>
+<!--DOCTYPE html>
 <html>
 <head>
     <title>Register Member</title>
@@ -39,4 +39,52 @@
     </form>
 
     </body>
+</html-->
+
+<!doctype html>
+<html lang="en">
+
+<head>
+    <title>Register | XStore</title>
+    <meta charset="utf-8">
+
+    <link rel="dns-prefetch" href="//fonts.googleapis.com">
+    <link href="https://fonts.googleapis.com/css?family=Rubik:300,400,500" rel="stylesheet">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+    
+    <link rel="stylesheet" type="text/css" href="css/login.css">
+    
+    <!--link href="css/style.css" rel="stylesheet"-->
+</head>  
+<body>
+
+    <div class="bg-gradient">
+        <div class="box">
+            <h2>REGISTER</h2>
+            <form action="" method="post">
+                <div class="inputBox">
+                    <input type="text" name="" required="">
+                    <label>Nama Depan</label>    
+                </div>
+                <div class="inputBox">
+                    <input type="text" name="" required="">
+                    <label>Nama Belakang</label>    
+                </div>
+                <div class="inputBox">
+                    <input type="Email" name="" required="">
+                    <label>Email</label>    
+                </div>
+                <div class="inputBox">
+                    <input type="password" name="" required="">
+                    <label>Password</label> 
+                </div>
+                
+                <p id="submit"><input type="submit" name="" value="Register"></p>  
+                
+            </form>
+        </div>
+        
+    </div>  
+        
+</body>
 </html>
