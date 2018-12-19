deb8907da04abac893bda8b027e0b573e2e51f62
Ivan Indrakusuma <ivan.17051@mhs.its.ac.id>
2018-12-19 11:19:17 +0700

    Revert "Fixed #4"
    This reverts commit c6fbdc0d3ee362c7f39e14fe2f9a03061d4d1ca7.

--- cartRule.php
+++ cartRule.php
@@ -2,7 +2,6 @@
     if(isset($_GET['id'])){
         $content = &$_SESSION['cart']['cart'];
         
-        
         if(array_key_exists($_GET["id"],$content)===FALSE){
             $content [$_GET['id']] = 1;
         }
